35f9d802ea116898e00a4bf9a8671a01ccc2edfe
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-25 09:21:50 +0100

    Add calculator + caesarCipher functions + tests

--- js-tests/index.js
+++ js-tests/index.js
@@ -7,3 +7,44 @@ export function capitalize(string) {
 export function reverseString(string) {
   return string.split("").reverse().join("");
 }
+
+export const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+};
+
+export function caesarCipher(string, key) {
+  const plainAlphabet = " .,?!abcdefghijklmnopqrstuvwxyz0123456789";
+  let cipherAlphabet = plainAlphabet.split("");
+  let keyLetters = undefined;
+  key = key % 41;
+  if (key < 0) {
+    keyLetters = cipherAlphabet.splice(key);
+    cipherAlphabet = keyLetters.concat(cipherAlphabet);
+  } else {
+    keyLetters = cipherAlphabet.splice(0, key);
+    cipherAlphabet = cipherAlphabet.concat(keyLetters);
+  }
+
+  const cipherString = [];
+
+  string
+    .toLowerCase()
+    .split("")
+    .forEach((element) => {
+      let index = plainAlphabet.split("").indexOf(element);
+      cipherString.push(cipherAlphabet[index]);
+    });
+
+  return cipherString.join("");
+}
--- js-tests/index.test.js
+++ js-tests/index.test.js
@@ -1,5 +1,7 @@
 import { capitalize } from "./index.js";
 import { reverseString } from "./index.js";
+import { calculator } from "./index.js";
+import { caesarCipher } from "./index.js";
 
 test("Return only first letter capitalized", () => {
   expect(capitalize("herman")).toBe("Herman");
@@ -7,4 +9,32 @@ test("Return only first letter capitalized", () => {
 
 test("Reverse string", () => {
   expect(reverseString("uwe")).toBe("ewu");
+});
+
+test("Adds 1 + 2 to equal 3", () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test("Subtract 3 - 2 to equal 1", () => {
+  expect(calculator.subtract(3, 2)).toBe(1);
+});
+
+test("Divide 6 / 2 to equal 3", () => {
+  expect(calculator.divide(6, 2)).toBe(3);
+});
+
+test("Multiply 4 * 2 to equal 8", () => {
+  expect(calculator.multiply(4, 2)).toBe(8);
+});
+
+test("Return Ab2 to be cd4 with key 2", () => {
+  expect(caesarCipher("Ab2", 2)).toBe("cd4");
+});
+
+test("Return Ab2 to be ?!0 with key -2", () => {
+  expect(caesarCipher("Ab2", -2)).toBe("?!0");
+});
+
+test("Return Ab2 to be de.5 with key 50", () => {
+  expect(caesarCipher("Ab2", 50)).toBe("jk.");
 });
