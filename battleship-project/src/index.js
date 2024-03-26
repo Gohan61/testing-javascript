db2d31ece74a44824d91dfe09950c1dfce3b63ea
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-26 10:27:26 +0100

    Feat: set timesHit by calling hit method

--- battleship-project/src/index.js
+++ battleship-project/src/index.js
@@ -4,4 +4,14 @@ export class Ship {
     this.timesHit = null;
     this.sunk = null;
   }
+
+  hit() {
+    this.timesHit += 1;
+  }
+
+  isSunk() {
+    if (this.length === this.timesHit) {
+      this.sunk = true;
+    }
+  }
 }
--- battleship-project/src/index.test.js
+++ battleship-project/src/index.test.js
@@ -2,13 +2,17 @@ import { Ship } from "./index.js";
 
 test("Times hit to be less than length of ship", () => {
   const testShip = new Ship(5);
-  testShip.timesHit = 4;
+  for (let i = 4; i > 0; i--) {
+    testShip.hit();
+  }
   expect(testShip.length).toBeGreaterThan(testShip.timesHit);
 });
 
 test("Ship is sunk when times hit equals length of ship", () => {
   const testShip = new Ship(5);
-  testShip.timesHit = 5;
+  for (let i = 5; i > 0; i--) {
+    testShip.hit();
+  }
   if (testShip.length === testShip.timesHit) {
     testShip.sunk = true;
   }
