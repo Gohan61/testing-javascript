826f6fa8b7b2b47493ce1b0dc0d7cc0c0cf5d9e9
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-03-28 18:24:22 +0100

    Feat: create dom elements, game grid + style
    * Set the basic html structure for the game
    * Created the grids for player and opponent
    * Change background color of opponent when selecting to attack

---
+++ battleship-project/src/dom.js
@@ -0,0 +1,37 @@
+import "./style.css";
+
+const computerBoard = document.querySelector(".computerGrid");
+const playerBoard = document.querySelector(".playerGrid");
+
+function createGrid(element) {
+  for (let i = 0; i < 100; i++) {
+    let div = document.createElement("div");
+    if (i < 10) {
+      div.setAttribute("data", `0${i}`);
+    } else {
+      div.setAttribute("data", i);
+    }
+    element.append(div);
+  }
+}
+
+createGrid(computerBoard);
+createGrid(playerBoard);
+
+const computerGridElements = document.querySelectorAll(".computerGrid div");
+
+function hover(element) {
+  element.forEach((div) => {
+    div.addEventListener("mouseover", (color) => {
+      color.currentTarget.style.backgroundColor = "blue";
+    });
+  });
+
+  element.forEach((div) => {
+    div.addEventListener("mouseout", (color) => {
+      color.currentTarget.style.backgroundColor = "";
+    });
+  });
+}
+
+hover(computerGridElements);
